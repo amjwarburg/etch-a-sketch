74464cfb0692a705130ec31c288a9f1487d1af15
amjwaburg <amjwarburg@gmail.com>
2023-08-19 22:07:04 +0100

    Add color mouseenter functionality
    This commit adds the functionality of the actual 'pen'. It uses
    Random.math to set up randomly coloured divs where the mouse hovers
    over.
    
    I now need to figure out a way of getting the canvas to resize and
    replace itself!

--- scripts.js
+++ scripts.js
@@ -14,9 +14,20 @@ function createPixels(num) {
             rows.appendChild(columns);
             columns.style.height = `${600/num}px`
             columns.style.width = `${600/num}px`
-        }
+            columns.addEventListener('mouseenter', () => {
+            columns.style.backgroundColor = `rgb(${Math.random()*256}, ${Math.random()*256}, ${Math.random()*256})`
+        });
         pad.appendChild(rows)
-    }
+     }
+  }
 }
+
+let rows = document.querySelector('.rows')
+let button = document.querySelector('.canvas')
+let container = document.querySelector('.container')
+button.addEventListener('click', () => { 
+    let num = prompt('Enter a size for the grid 10-100')
+    createPixels(num);
+})
 
 createPixels(16)
