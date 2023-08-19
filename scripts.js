3fad87a16af9b042a4d54baa33b4a1d2b1b473a2
amjwaburg <amjwarburg@gmail.com>
2023-08-19 18:50:06 +0100

    Add in div grid and size functionality
    This commit adds a lot of functionality for not much code! First,
    a grid was created using loops and various DOM changing elements.
    
    Then, the ability to alter the size of the grid was added. But the
    crucial part was ensuring that the size of the box remains the
    same, whilst the number of pixels increases. This was achieved
    using a ${} placeholder with the number of 'px' coming at the end
    (outside of the placeholder brackets).

--- scripts.js
+++ scripts.js
@@ -1,4 +1,22 @@
-const pad = document.querySelector('.sketch-pad');
-const pixel = document.createElement('div');
-pixel.classList.add('pixel');
-pad.appendChild(pixel);
+let pad = document.querySelector('.sketch-pad')
+
+function createPixels(num) {
+    for (let rows = 0; rows < num; rows++) {
+        let rows = document.createElement('div');
+        rows.classList.add('rows');
+        pad.appendChild(rows);
+        rows.style.height = `${600/num}px`
+        rows.style.width = `${600/num}px`
+        
+        for (let columns = 0; columns < num; columns++) {
+            let columns = document.createElement('div');
+            columns.classList.add('columns');
+            rows.appendChild(columns);
+            columns.style.height = `${600/num}px`
+            columns.style.width = `${600/num}px`
+        }
+        pad.appendChild(rows)
+    }
+}
+
+createPixels(16)
